cd6c13328597a51663f9f8bf17d69d8665d0c6db
Dominik <dlenda@o2.pl>
2018-04-30 16:48:31 +0200

    2

---
+++ scripts/gatsby-config.js
@@ -0,0 +1,11 @@
+plugins: [
+    {
+        resolve: `gatsby-plugin-nprogress`,
+        options: {
+            // Setting a color is optional.
+            color: `red`,
+            // Disable the loading spinner.
+            showSpinner: false,
+        },
+    },
+];
