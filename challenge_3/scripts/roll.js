38623aeb05938653a87151a9729db2d156261333
Dominik <dlenda@o2.pl>
2018-05-01 12:52:18 +0200

    2

---
+++ challenge_3/scripts/roll.js
@@ -0,0 +1,121 @@
+var item1 = $(".gallery__rolls1");
+var item2 = $(".gallery__rolls2");
+var item3 = $(".gallery__rolls3");
+var item4 = $(".gallery__rolls4");
+var item5 = $(".gallery__rolls5");
+
+$(function() {
+// OPACITY OF BUTTON SET TO 0%
+    item1.css("opacity","0");
+
+
+// ON MOUSE OVER
+    item1.hover(function () {
+
+// SET OPACITY TO 70%
+            $(this).stop().animate({
+                opacity: .7
+            }, "slow");
+        },
+
+// ON MOUSE OUT
+        function () {
+
+// SET OPACITY BACK TO 50%
+            $(this).stop().animate({
+                opacity: 0
+            }, "slow");
+        });
+});
+$(function() {
+// OPACITY OF BUTTON SET TO 0%
+    item2.css("opacity","0");
+
+
+// ON MOUSE OVER
+    item2.hover(function () {
+
+// SET OPACITY TO 70%
+            $(this).stop().animate({
+                opacity: .7
+            }, "slow");
+        },
+
+// ON MOUSE OUT
+        function () {
+
+// SET OPACITY BACK TO 50%
+            $(this).stop().animate({
+                opacity: 0
+            }, "slow");
+        });
+});
+$(function() {
+// OPACITY OF BUTTON SET TO 0%
+    item3.css("opacity","0");
+
+
+// ON MOUSE OVER
+    item3.hover(function () {
+
+// SET OPACITY TO 70%
+            $(this).stop().animate({
+                opacity: .7
+            }, "slow");
+        },
+
+// ON MOUSE OUT
+        function () {
+
+// SET OPACITY BACK TO 50%
+            $(this).stop().animate({
+                opacity: 0
+            }, "slow");
+        });
+});
+$(function() {
+// OPACITY OF BUTTON SET TO 0%
+    item4.css("opacity","0");
+
+
+// ON MOUSE OVER
+    item4.hover(function () {
+
+// SET OPACITY TO 70%
+            $(this).stop().animate({
+                opacity: .7
+            }, "slow");
+        },
+
+// ON MOUSE OUT
+        function () {
+
+// SET OPACITY BACK TO 50%
+            $(this).stop().animate({
+                opacity: 0
+            }, "slow");
+        });
+});
+$(function() {
+// OPACITY OF BUTTON SET TO 0%
+    item5.css("opacity","0");
+
+
+// ON MOUSE OVER
+    item5.hover(function () {
+
+// SET OPACITY TO 70%
+            $(this).stop().animate({
+                opacity: .7
+            }, "slow");
+        },
+
+// ON MOUSE OUT
+        function () {
+
+// SET OPACITY BACK TO 50%
+            $(this).stop().animate({
+                opacity: 0
+            }, "slow");
+        });
+});
---
+++ challenge_3/scripts/slick_custom.js
@@ -0,0 +1,9 @@
+$(document).on('ready', function() {
+
+    $(".titleSection__slider").slick({
+        lazyLoad: 'ondemand', // ondemand progressive anticipated
+        infinite: true,
+        //autoplay: true,
+
+    });
+});
