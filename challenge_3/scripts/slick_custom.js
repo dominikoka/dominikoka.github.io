2f94b55846166c3aaa3b12e0e6565f3576e0c44f
Dominik <dlenda@o2.pl>
2018-05-04 13:03:52 +0200

    dsf

--- challenge_3/scripts/slick_custom.js
+++ challenge_3/scripts/slick_custom.js
@@ -3,7 +3,7 @@ $(document).on('ready', function() {
     $(".titleSection__slider").slick({
         lazyLoad: 'ondemand', // ondemand progressive anticipated
         infinite: true,
-        //autoplay: true,
+        autoplay: true,
 
     });
 });
