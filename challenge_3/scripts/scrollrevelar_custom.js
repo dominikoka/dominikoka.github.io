73c91f89abd412fece7b85fef9b9b87c55084513
Dominik <dlenda@o2.pl>
2018-05-04 20:09:08 +0200

    correct_h reset_scroll

--- challenge_3/scripts/scrollrevelar_custom.js
+++ challenge_3/scripts/scrollrevelar_custom.js
@@ -1,26 +1,26 @@
 window.sr = ScrollReveal();
-sr.reveal('.relax__boxText', { duration: 800,origin: 'right', distance: '200px',reset: true,
+sr.reveal('.relax__boxText', { duration: 800,origin: 'right', distance: '200px',
 });
-sr.reveal('.relax__items', { duration: 1500,origin: 'right', distance: '200px',reset: true,delay: 500
+sr.reveal('.relax__items', { duration: 1500,origin: 'right', distance: '200px',delay: 500
 });
-sr.reveal('.relax__button', { duration: 1500,origin: 'right', distance: '200px',reset: true,delay: 1000
+sr.reveal('.relax__button', { duration: 1500,origin: 'right', distance: '200px',delay: 1000
 });
-sr.reveal('client__circle', { duration: 700,origin: 'left', distance: '200px',reset: true,
-});
-
-
-sr.reveal('.client__background', { duration: 1500,origin: 'right', distance: '200px',reset: true,
-});
-
-sr.reveal('.ready__title', { duration: 900,origin: 'left', distance: '200px',reset: true,
+sr.reveal('client__circle', { duration: 700,origin: 'left', distance: '200px',
 });
 
 
-
-sr.reveal('.project__itemImg', { duration: 900,origin: 'left', distance: '100px',reset: true,
-});
-sr.reveal('.project__itemText', { duration: 900,origin: 'right', distance: '100px',reset: true,
+sr.reveal('.client__background', { duration: 1500,origin: 'right', distance: '200px',
 });
 
-sr.reveal('.values__itemImg', { duration: 900,origin: 'top', distance: '100px',reset: true,
+sr.reveal('.ready__title', { duration: 900,origin: 'left', distance: '200px',
+});
+
+
+
+sr.reveal('.project__itemImg', { duration: 900,origin: 'left', distance: '100px',
+});
+sr.reveal('.project__itemText', { duration: 900,origin: 'right', distance: '100px',
+});
+
+sr.reveal('.values__itemImg', { duration: 900,origin: 'top', distance: '100px',
 });
